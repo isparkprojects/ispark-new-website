3c8514d657cee50c618d08d9429da83735045c21
isparkprojects <isparkdevelopers@gmail.com>
2024-10-15 14:28:11 +0530

    Modified about section

--- about.js
+++ about.js
@@ -24,3 +24,40 @@ window.addEventListener('scroll', () => {
         });
     }
 });
+
+
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    let elements = document.querySelectorAll('.scroll-element');
+
+    elements.forEach(function(el) {
+        let elementTop = el.getBoundingClientRect().top + window.scrollY;
+        let elementBottom = el.getBoundingClientRect().bottom + window.scrollY;
+
+        if (elementTop < currentScroll + window.innerHeight && elementBottom > currentScroll) {
+            // When the element is visible in the viewport
+            if (currentScroll > lastScrollTop) {
+                // Scrolling down - slide in from the left
+                el.classList.add('show-left');
+                el.classList.remove('show-right', 'hide-left', 'hide-right');
+            } else {
+                // Scrolling up - slide in from the right
+                el.classList.add('show-right');
+                el.classList.remove('show-left', 'hide-left', 'hide-right');
+            }
+        } else {
+            // Reset to allow looping when out of view
+            if (currentScroll > lastScrollTop) {
+                el.classList.add('hide-left'); // Reset to off-left for scrolling down
+                el.classList.remove('show-left', 'show-right', 'hide-right');
+            } else {
+                el.classList.add('hide-right'); // Reset to off-right for scrolling up
+                el.classList.remove('show-left', 'show-right', 'hide-left');
+            }
+        }
+    });
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
